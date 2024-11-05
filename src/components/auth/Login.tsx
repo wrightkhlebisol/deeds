a1bb41dc179e85a8c1cd2d6d3cd796c458f5020f
wrightkhlebisol <calebogundiya@gmail.com>
2024-11-05 18:15:14 +0000

    feat: login modal component

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,43 @@
+import { Field, Fieldset, Input, Label, Legend, Button } from '@headlessui/react';
+import { useState } from 'react';
+import clsx from 'clsx';
+import Modal from '../Modal';
+
+export default function Login() {
+  const [open, setOpen] = useState(false);
+  return (
+
+    <Modal open={open} setOpen={setOpen}>
+      <form className="w-full max-w-lg">
+        <Fieldset className="space-y-3 rounded-xl bg-black/5 p-6 sm:p-10">
+          <Legend className="text-base/3 font-semibold text-black">Login</Legend>
+          <div className="h-px bg-gray-300" />
+          <Field>
+            <Label className="text-sm/6 font-medium">Email/Username</Label>
+            <Input
+              className={clsx(
+                'mt-3 block w-full rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-black',
+                'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25'
+              )}
+            />
+          </Field>
+          <Field>
+            <Label className="text-sm/6 font-medium text-black">Password</Label>
+            <Input
+              className={clsx(
+                'mt-3 block w-full rounded-lg border-none bg-black/5 py-1.5 px-3 text-sm/6 text-black',
+                'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25'
+              )}
+              type='password' />
+          </Field>
+          <Field className="flex gap-5">
+            <Button
+              className="w-8/12 bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 text-sm/6 font-semibold"
+              type='submit'>Login</Button>
+            <p className="w-4/12 text-blue-500 hover:text-blue-400 rounded-lg py-2 text-sm/6 cursor-pointer">Register Instead</p>
+          </Field>
+        </Fieldset>
+      </form>
+    </Modal>
+  )
+}
